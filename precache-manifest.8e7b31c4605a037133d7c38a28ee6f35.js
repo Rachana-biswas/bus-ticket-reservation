0ced6720f2ead3e8a8a206c03c9520609cbde508
Rachana-biswas <fchakraborty51@gmail.com>
2020-09-07 15:49:20 +0530

    Updates

---
+++ precache-manifest.8e7b31c4605a037133d7c38a28ee6f35.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a832559120015642ca118bf0a2a4edf8",
+    "url": "/bus-ticket-reservation/index.html"
+  },
+  {
+    "revision": "e89036c595a73aef5bc2",
+    "url": "/bus-ticket-reservation/static/css/main.70325f72.chunk.css"
+  },
+  {
+    "revision": "c337bacc7cc37faf8933",
+    "url": "/bus-ticket-reservation/static/js/2.4b288684.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bus-ticket-reservation/static/js/2.4b288684.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e89036c595a73aef5bc2",
+    "url": "/bus-ticket-reservation/static/js/main.ee82bfab.chunk.js"
+  },
+  {
+    "revision": "02dbaec6116784810f90",
+    "url": "/bus-ticket-reservation/static/js/runtime-main.a84e7bc4.js"
+  },
+  {
+    "revision": "bd78049dff5bedb473cd6a9b889be22b",
+    "url": "/bus-ticket-reservation/static/media/buscopy1.bd78049d.jpg"
+  },
+  {
+    "revision": "31082b8e91d8a8a4e6c1db828e2d02fb",
+    "url": "/bus-ticket-reservation/static/media/buscopynew.31082b8e.jpg"
+  },
+  {
+    "revision": "ff115095b292c6359426e284714e9918",
+    "url": "/bus-ticket-reservation/static/media/logg.ff115095.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bus-ticket-reservation/static/media/logo.5d5d9eef.svg"
+  }
+]);
